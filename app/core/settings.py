ae7ac433a5b473ebd0a3b96d25d6c9ae50ade736
VarunVashisth <varun123shivam@gmail.com>
2026-06-16 00:54:20 +0530

    mkc

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,8 +5,6 @@ from pydantic_settings import BaseSettings , SettingsConfigDict
 class Settings(BaseSettings):
 
     DATABASE_URL : str
-    SMTP_EMAIL: str
-    SMTP_PASSWORD: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
